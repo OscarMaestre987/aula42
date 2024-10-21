7376874241ff4dd7b130c7326b4f8024e6ae5533
OscarMaestre987 <118920861+OscarMaestre987@users.noreply.github.com>
2024-10-21 10:31:53 +0200

    cambio

--- 42 Madrid/ejercicio1/main.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-#include <stdio.h>
-
-int main(void)
-{
-    printf("ft_isalpha('A'): %d\n", ft_isalpha('A')); // Espera 1
-    printf("ft_strlen(\"Hola\"): %lu\n", (unsigned long)ft_strlen("Hola")); // Espera 4
-    return (0);
-}
