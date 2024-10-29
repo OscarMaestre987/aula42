daf91a785e6b92d3c8438ccb05c4238d772b7a84
OscarMaestre987 <118920861+OscarMaestre987@users.noreply.github.com>
2024-10-29 10:22:03 +0100

    a

--- libft.a/ft_strchr.c
+++ libft.a/ft_strchr.c
@@ -14,13 +14,16 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s != '\0')
+	unsigned char	uc;
+
+	uc = (unsigned char)(c % 256);
+	while (*s)
 	{
-		if (*s == (char)c)
+		if (*s == uc)
 			return ((char *)s);
 		s++;
 	}
-	if (c == 0)
+	if (uc == '\0')
 		return ((char *)s);
 	return (NULL);
 }
